1927062b29aac54bc5c53df26813d3476b6ad0e9
Ameer Hamza <ameerhamza.dev0@gmail.com>
2026-05-08 20:31:51 +0500

    Day_12: Pattern Practice

---
+++ Week_02/Day_12_pattern practice/Inverted pyramid.py
@@ -0,0 +1,3 @@
+n = 3
+for i in range(n, 0, -1):
+    print("  " * (n - i) + "* " * (2 * i - 1))
---
+++ Week_02/Day_12_pattern practice/Number pyramid.py
@@ -0,0 +1,6 @@
+n = 3
+for i in range(1, n + 1):
+    print("  " * (n - i), end="")
+    for j in range(1, i + 1): print(j, end=" ") # Forward
+    for j in range(i - 1, 0, -1): print(j, end=" ") # Backward
+    print()
