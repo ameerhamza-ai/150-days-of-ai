3271bb25f846621b9d5479031be0f363a0ef1644
Ameer Hamza <ameerhamza.dev0@gmail.com>
2026-05-10 19:36:36 +0500

    Refactor: Update attempt logic and add exit functionality to Guessing Game

--- Week_02/Day_14_review/Number Guessing Game.py
+++ Week_02/Day_14_review/Number Guessing Game.py
@@ -1,15 +1,21 @@
-# Number Guessing Game
-
 import random
 
 secret = random.randint(1, 10)
 attempts = 3
 
 print("--- Number Guessing Game (1 to 10) ---")
+print("Type '0' to exit the game anytime.")
 
 while attempts > 0:
     print(f"\nAttempts remaining: {attempts}")
-    guess = int(input("Guess the number: "))
+    user_input = input("Guess the number: ")
+
+    
+    if user_input == '0':
+        print("Exiting game... Goodbye!")
+        break
+    guess = int(user_input)
+    attempts -= 1  
 
     if guess == secret:
         print("Winner! ")
@@ -18,7 +24,6 @@ while attempts > 0:
         print("Too high!")
     else:
         print("Too low!")
-        attempts -= 1  
 
-if attempts == 0:
+if attempts == 0 and guess != secret:
     print(f"\nBetter luck next time! Answer was: {secret}")
